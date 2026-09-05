5039e3d9b864698ff5810d3fea3a2896fdb773bb
chgor11 <chgor1@outlook.com>
2026-09-05 17:12:42 +0330

    Update FinalizeActivity.java

--- app/src/main/java/net/typeblog/shelter/ui/FinalizeActivity.java
+++ app/src/main/java/net/typeblog/shelter/ui/FinalizeActivity.java
@@ -6,8 +6,7 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class FinalizeActivity extends AppCompatActivity {
-    @Override
+public class FinalizeActivity extends SecureActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Intent i = new Intent(getApplicationContext(), DummyActivity.class);
