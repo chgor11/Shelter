a60f64ab0f3851920d5042efc9dcf6964a71e1da
chgor11 <chgor1@outlook.com>
2026-09-07 12:12:56 +0330

    Update DevicePolicyManagerFragment.java

--- app/src/main/java/net/typeblog/shelter/ui/DevicePolicyManagerFragment.java
+++ app/src/main/java/net/typeblog/shelter/ui/DevicePolicyManagerFragment.java
@@ -432,8 +432,6 @@ public class DevicePolicyManagerFragment
 
     private void requestAuthentication() {
 
-        android.util.Log.i("ShelterMaxSecurity", "PARENT: requestAuthentication() entered");
-
     /*
      * SECURITY POLICY AUTHENTICATION
      *
@@ -541,8 +539,6 @@ public class DevicePolicyManagerFragment
         if (requestCode ==
                 REQUEST_POLICY_AUTH) {
 
-            android.util.Log.i("ShelterMaxSecurity", "PARENT: authentication result=" + resultCode);
-
             if (resultCode ==
                     android.app.Activity.RESULT_OK) {
 
@@ -550,7 +546,6 @@ public class DevicePolicyManagerFragment
                  * Authentication is valid only
                  * for this current transaction.
                  */
-                android.util.Log.i("ShelterMaxSecurity", "PARENT: authentication SUCCESS; authorizing transaction");
                 mChangeManager
                         .authorizeCurrentTransaction();
 
@@ -569,7 +564,6 @@ public class DevicePolicyManagerFragment
                  *
                  * No pending operation is executed.
                  */
-                android.util.Log.w("ShelterMaxSecurity", "PARENT: authentication CANCELED/FAILED");
                 mChangeManager
                         .clearAuthenticationSession();
             }
