205c8ca63e73bfa922309b595998f2f71b756dbf
chgor11 <chgor1@outlook.com>
2026-09-13 07:23:33 +0330

    Update SystemPageSecurityGuard.java

--- app/src/main/java/net/typeblog/shelter/security/SystemPageSecurityGuard.java
+++ app/src/main/java/net/typeblog/shelter/security/SystemPageSecurityGuard.java
@@ -31,7 +31,7 @@ import net.typeblog.shelter.receivers.ShelterDeviceAdminReceiver;
  */
 public final class SystemPageSecurityGuard {
     private static final String TAG = "ShelterPageSecurity";
-    private static final long GRACE_PERIOD_MS = 1L * 20L * 1000L;
+    private static final long GRACE_PERIOD_MS = 1L * 30L * 1000L;
 
     private final Context mContext;
     private final DevicePolicyManager mDpm;
